b877998678986101dd3c8affdb2ad49cc625b675
sopel39 <napewnotrafi@gmail.com>
2013-11-21 22:17:20 +0100

    bitrate selection, fill wav support

--- ffmpeg.js/emcc-audio-config.sh
+++ ffmpeg.js/emcc-audio-config.sh
@@ -4,7 +4,7 @@
  --cpu=generic --disable-hwaccels --disable-stripping --disable-pthreads --disable-doc --disable-debug --disable-asm\
  --disable-network --disable-everything --disable-ffplay --disable-ffprobe --disable-ffserver --disable-outdev=sdl\
  --enable-optimizations --enable-protocol=file  --enable-filter=aresample\
- --enable-demuxer=wav --enable-decoder=pcm_s16le\
+ --enable-demuxer=wav --enable-decoder=pcm_s16le --enable-decoder=pcm_u8\
  --enable-libmp3lame --enable-demuxer=mp3 --enable-decoder=mp3 --enable-encoder=libmp3lame --enable-muxer=mp3\
  --enable-libvorbis --enable-demuxer=ogg --enable-decoder=libvorbis --enable-encoder=libvorbis --enable-muxer=ogg\
  --enable-libfdk-aac --enable-demuxer=mov --enable-demuxer=aac --enable-decoder=aac --enable-encoder=libfdk_aac --enable-muxer=mp4\
